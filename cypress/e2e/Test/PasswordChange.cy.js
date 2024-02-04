fdd80f558fd1d305105e472ec8bc98f4d2d24028
ashraful2430 <www.savage582@gmail.com>
2024-02-04 13:47:52 +0600

    created a pasword change test

---
+++ cypress/e2e/Test/PasswordChange.cy.js
@@ -0,0 +1,13 @@
+describe("Changing Password", () => {
+  it("Let the user change paswword", () => {
+    cy.request({
+      method: "PATCH",
+      url: "https://staging-api.biyekorun.us/user/changepassword/ashrafulislamashik960@gmail.com",
+      body: {
+        currentPassword: "Ashik@2430",
+        newPassword: "Ashik@2431",
+        confirmPassword: "Ashik@2431",
+      },
+    }).its("status").should("equal", 200)
+  });
+});
