efe28c41c2a6a4024e049c80e46cd641812b479a
Mitul das <mituldas751@gmail.com>
2024-02-05 16:28:55 +0600

    add chats api on testing

---
+++ cypress/e2e/Backend/API/UserFlow/1.35-ChatAPI/2.1-CreateChat/createChat.cy.js
@@ -0,0 +1,18 @@
+describe("As an user, I should be able to create Initital chat", () => {
+    it("Checking if a user can create Initital chat or not", () => {
+      const accessToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWMwNDUyZmViZjUzYWNiZWYyNzYwNmUiLCJlbWFpbCI6ImFzaHJhZnVsaXNsYW1hc2hpazk2MEBnbWFpbC5jb20iLCJ1c2VySWQiOiJCS0xTOEFYSFNDIiwiZmlyc3ROYW1lIjoiQXNocmFmdWwgSXNsYW0iLCJsYXN0TmFtZSI6IkFzaGlrIiwicm9sZSI6InVzZXIiLCJnZW5kZXIiOiJNYWxlIiwicGFja2FnZSI6IjY1YzA0NTJmZWJmNTNhY2JlZjI3NjA2YyIsImlhdCI6MTcwNzExNjMzNCwiZXhwIjoxNzA3MjAyNzM0fQ.Q0560L8MilPeGRXztwhdFWsHATVTWon_OuoQ-jR5fbc";
+      cy.request({
+        method: "POST",
+        url: "/api/chat/findorcreate/658cfb6823934d606b72b3ac",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: [
+
+        ],
+      })
+        .its("status")
+        .should("equal", 200);
+    });
+  });
