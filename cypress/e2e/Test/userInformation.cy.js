9f6497815ad833883c1d84521010cf6c20ecc131
ashraful2430 <www.savage582@gmail.com>
2024-02-10 07:33:09 +0600

    Fixed some test files

--- cypress/e2e/Test/userInformation.cy.js
+++
@@ -1,5 +0,0 @@
-describe("User Information", () => {
-  it("Geeting all user information", () => {
-    cy.request("GET", "/api/user").its("status").should("equal", 200);
-  });
-});
--- cypress/e2e/Test/userVerify.cy.js
+++
@@ -1,5 +0,0 @@
-describe('User Verification', () => { 
-    it('Seeing if the user is verified or not', () => {
-        cy.request()
-    });
- })
