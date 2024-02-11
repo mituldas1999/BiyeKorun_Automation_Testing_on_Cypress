96f916d1d2cd7d3ec8cf99f2166617f38a80b5a4
ashraful2430 <www.savage582@gmail.com>
2024-02-11 10:50:16 +0600

    tested some otp apis from swigger

--- cypress/e2e/Backend/API/UserFlow/0.4-Delete_OTP/deleteOtp.cy.js
+++ cypress/e2e/Backend/API/UserFlow/0.4-Delete_OTP/deleteOtp.cy.js
@@ -1,14 +1,13 @@
-describe("As an user I can send an OTP with email", () => {
-  it.skip("Checking if an user can send OTP or not", () => {
+describe("As a user I can send OTP for deleted user", () => {
+  it("Checking if an deleted user can get API or not", () => {
     const accessToken = Cypress.env("accessToken");
     cy.request({
       method: "POST",
-      url: "/api/user/resendotp",
+      url: "/api/user/delete-otp",
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
       body: {
-        email: "ashrafulislamashik960@gmail.com",
         otpType: "email",
       },
     }).then((response) => {
--- cypress/e2e/Backend/API/UserFlow/1.35-ChatAPI/2.15-RemoveUser/removeUser.cy.js
+++ cypress/e2e/Backend/API/UserFlow/1.35-ChatAPI/2.15-RemoveUser/removeUser.cy.js
@@ -1,1 +1,24 @@
-describe('first', () => { second })
+describe("As an user I can remove a user from channel", () => {
+  let newUserId; // Declare newUserId variable
+  before(() => {
+    cy.readFile("cypress/fixtures/userId.json").then((data) => {
+      newUserId = data.userId; // Store the group ID in the variable
+      console.log(newUserId);
+    });
+  });
+  it("Checking if an user can remove an user from a channel or not", () => {
+    const accessToken = Cypress.env("accessToken");
+    cy.request({
+      method: "PATCH",
+      url: `/api/chat/channel/remove-user/${newUserId}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: {
+        member: `${newUserId}`,
+      },
+    }).then((response) => {
+      console.log(response.body);
+    });
+  });
+});
