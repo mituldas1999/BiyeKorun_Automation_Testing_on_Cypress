197377e01ff0b1996aa276de6619fe075a62e6ae
ashraful2430 <www.savage582@gmail.com>
2024-02-15 11:45:34 +0600

    blah blah

--- runTest.js
+++ runTest.js
@@ -2,21 +2,34 @@
 const cypress = require("cypress");
 
 async function runTests() {
+  /*************** Admin Start *****************************/
+  //   await cypress.run({
+  //     spec: "cypress/e2e/Backend/API/1-AdminFlow/",
+  //   });
+  /****************** Admin End ***********************************/
+  /*************** Package Start *****************************/
+  //   await cypress.run({
+  //     spec: "cypress/e2e/Backend/API/2-Packages/",
+  //   });
+  /*************** Package End *****************************/
+
+  /*************** Public API Start *****************************/
   await cypress.run({
-    spec: "cypress/e2e/Backend/API/1-AdminFlow/",
+    spec: "cypress/e2e/Backend/API/3-PublicAPI/",
   });
-  await cypress.run({
-    spec: "cypress/e2e/Backend/API/2-Packages/",
-  });
-  await cypress.run({
-    spec: "cypress/e2e/Backend/API/3-Payment/",
-  });
-  await cypress.run({
-    spec: "cypress/e2e/Backend/API/4-UserFlow/",
-  });
-  await cypress.run({
-    spec: "cypress/e2e/Backend/API/5-PublicAPI/",
-  });
+  /*************** Public API End *****************************/
+
+  /***************** User Flow Start ************************************/
+  //     await cypress.run({
+  //       spec: "cypress/e2e/Backend/API/4-UserFlow/0.1-Register/",
+  //     });
+  //     await cypress.run({
+  //       spec: "cypress/e2e/Backend/API/4-UserFlow/0.2-SendOTP/",
+  //     });
+  //   await cypress.run({
+  //     spec: "cypress/e2e/Backend/API/4-UserFlow/1.35-ChatAPI/",
+  //   });
+  /*************** User Flow End *****************************/
 }
 
 runTests();
