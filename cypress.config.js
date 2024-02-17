3140a66e9fae1e50d6b31b2abd9c2faac8b324d7
Mitul Das <59419400+mituldas1999@users.noreply.github.com>
2024-02-17 10:54:00 +0600

    Add reports on cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,16 +3,12 @@ const { defineConfig } = require("cypress");
 require("dotenv").config();
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   projectId: "8axud3",
   e2e: {
     baseUrl: "https://staging-api.biyekorun.us",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // config.specPattern = [
-      //   "cypress/e2e/API/AdminFlow/1-Admin[Auth]/1.1-Admin_login/adminLogin.cy.js",
-      //   "cypress/e2e/API/AdminFlow/1-Admin[Auth]/1.2-Edit_Profile/editProfile.cy.js",
-      // ];
-      // return config;
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
