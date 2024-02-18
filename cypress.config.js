6705e4608c2448a4be3ad508f079b66d08fde70f
ashraful2430 <www.savage582@gmail.com>
2024-02-18 11:06:57 +0600

    tried to user mocah merge

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,19 @@ const { defineConfig } = require("cypress");
 require("dotenv").config();
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
   projectId: "8axud3",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     baseUrl: "https://staging-api.biyekorun.us",
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
